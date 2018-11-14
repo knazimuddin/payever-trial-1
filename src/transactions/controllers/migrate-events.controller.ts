a2c4ae9df1b05234ca6909224d58afce4aa4beaa
Aleksey Bulash <alexeybulash@gmail.com>
2018-11-14 16:14:18 +0200

    TM-9 - dummy redeploy

--- src/transactions/controllers/migrate-events.controller.ts
+++ src/transactions/controllers/migrate-events.controller.ts
@@ -33,32 +33,4 @@ export class MigrateEventsController {
     this.transactionsService.createOrUpdate(transaction);
   }
 
-  // private prepareTransactionForInsert(transaction) {
-    // transaction.billing_address = transaction.address;
-    // transaction.original_id = transaction.id;
-    // transaction.type = transaction.payment_type;
-    // transaction.payment_details = JSON.stringify(transaction.payment_details);
-
-    // if (transaction.business) {
-      // transaction.business_uuid = transaction.business.uuid;
-      // transaction.merchant_name = transaction.business.company_name;
-      // transaction.merchant_email = transaction.business.company_email;
-    // }
-
-    // if (transaction.history && transaction.history.length) {
-      // const updatedHistory = transaction.history.map((historyItem) => {
-        // return this.prepareTransactionHistoryItemForInsert(historyItem);
-      // });
-    // }
-  // }
-
-  // private prepareTransactionHistoryItemForInsert(data) {
-    // return {
-      // ...data.data,
-      // action: data.history_type,
-      // created_at: Date.now(),
-      // is_restock_items: data.items_restocked,
-    // };
-  // }
-
 }
