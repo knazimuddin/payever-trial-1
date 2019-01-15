64f12a80c73b79069d36f762c8bbeef0a7eecc96
Oleg Mishenkin <olegm1990@mail.ru>
2019-01-15 15:34:27 +0300

    TM-31 Remove constraints on credentials BPO property

--- src/transactions/schemas/business-payment-option.schema.ts
+++ src/transactions/schemas/business-payment-option.schema.ts
@@ -1,13 +1,5 @@
 import { Schema } from 'mongoose';
 
-const CredentialsSchema = new Schema({
-  vendorNumber: String,
-  password: String,
-  isDownPaymentAllowed: Boolean,
-  isEmailNotificationAllowed: Boolean,
-  clickAndCollect: Boolean,
-});
-
 export const BusinessPaymentOptionSchema = new Schema({
   payment_option_id: Number,
   id: Number,
@@ -15,13 +7,12 @@ export const BusinessPaymentOptionSchema = new Schema({
   status: String,
   fixed_fee: Number,
   variable_fee: Number,
-  credentials: CredentialsSchema,
+  credentials: Schema.Types.Mixed,
   options: String, // json of options array
   completed: Boolean,
   shop_redirect_enabled: Boolean,
   uuid: String,
 });
-
 
 // {
   // payment_option_id: 9,
