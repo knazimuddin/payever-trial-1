551d2fa140c47c6728aec7e56500744129e64c93
Alexey Sitka <alexey.sitka@gmail.com>
2019-01-15 21:39:59 +0200

    TM-32 Updated packages.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { JwtAuthModule } from '@pe/nest-kit/modules/auth';
 
 import { environment } from './environments';
-import { TransactionsModule } from './transactions/transactions.module';
 import { StatusModule } from './status/status.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
     JwtAuthModule.forRoot(environment.jwtOptions),
     TransactionsModule,
-    MongooseModule.forRoot(environment.mongodb),
+    MongooseModule.forRoot(
+      environment.mongodb,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+      },
+    ),
     StatusModule,
   ],
   providers: [
