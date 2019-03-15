053e165c46bd6150154bdddc5d8c878b5265f07c
Alexey Sitka <alexey.sitka@gmail.com>
2019-03-15 16:17:51 +0200

    TM-87 Refactored payment-flow service.

--- src/transactions/services/payment-flow.service.ts
+++ src/transactions/services/payment-flow.service.ts
@@ -1,49 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { PaymentFlowInterface } from '../interfaces';
+import { PaymentFlowModel } from '../models';
 
 @Injectable()
 export class PaymentFlowService {
 
   constructor(
-    @InjectModel('PaymentFlow') private readonly model: Model<any>,
+    @InjectModel('PaymentFlow') private readonly model: Model<PaymentFlowModel>,
   ) {}
 
-  public async createOrUpdate(flow: any) {
-    if (flow.id) {
-      flow = this.wrap(flow);
-      const existing = await this.model.findOne({id: flow.id});
-      if (existing) {
-        return this.model.findOneAndUpdate({id: flow.id}, flow);
-      } else {
-        return this.model.create(flow);
-      }
-    }
+  public async createOrUpdate(flowDto: PaymentFlowInterface): Promise<PaymentFlowModel> {
+    const dto = {
+      // _id: flowDto.id,
+      ...flowDto,
+    };
+
+    await this.model.updateOne(
+      {
+        id: flowDto.id,
+      },
+      {
+        $setOnInsert: {
+          // _id: flowDto.id,
+        },
+        $set: dto,
+      },
+      {
+        upsert: true,
+      },
+    );
+
+    return this.findOneById(flowDto.id);
   }
 
-  public async findOne(id: string) {
-    return this.findOneByParams({id});
+  public async findOneById(id: string): Promise<PaymentFlowModel> {
+    const flow: PaymentFlowModel = await this.model.findOne({id});
+
+    return flow
+      ? flow.toObject({ virtuals: true })
+      : null
+    ;
   }
 
-  public async findOneByParams(params) {
-    const flow = await this.model.findOne(params);
-
-    return flow ? this.unwrap(flow.toObject({virtuals: true})) : null;
-  }
-
-  public async removeById(id: string) {
-    return this.model.findOneAndRemove({id});
-  }
-
-  private async create(flow: any) {
-    return this.model.create(flow);
-  }
-
-  private wrap(flow) {
-    return flow;
-  }
-
-  private unwrap(flow) {
-    return flow;
+  public async removeById(id: string): Promise<void> {
+    await this.model.findOneAndRemove({id});
   }
 }
