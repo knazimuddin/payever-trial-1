eebb5ccf7f082387247794709440b9ca49b4983a
Alexey Pimenov <a.pimenov93@gmail.com>
2019-04-02 13:56:23 +0300

    TM-106 es connection to env

--- src/transactions/es-temp/transactions-search.ts
+++ src/transactions/es-temp/transactions-search.ts
@@ -1,8 +1,9 @@
 // needs to be replaced with service, currently I have crash by using nest's es module,
 // needs to be investigated / replacing with custom.
+import { environment } from '../../environments';
 
 import * as elasticsearch from 'elasticsearch'
 export const client = new elasticsearch.Client({
-  host: '10.0.0.28:9200',
+  host: environment.es,
   log: 'error'
 });
