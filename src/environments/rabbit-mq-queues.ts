22d872cf0425f7967be96ba8f88875181fb9b390
Alexey Sitka <alexey.sitka@gmail.com>
2019-04-12 11:25:40 +0200

    TM-87 Refactored RabbitQueue config.

--- src/environments/rabbit-mq-queues.ts
+++ src/environments/rabbit-mq-queues.ts
@@ -1,70 +1,61 @@
-import { rabbitMqBindings } from './rabbit-mq-bindings';
+import { RabbitRoutingKeys } from '../enums';
 
 export const rabbitMqQueues = [
-  {
-    name: 'rpc_payment_santander_de',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_santander_invoice_de',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_factoring_de',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_santander_no',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_santander_dk',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_santander_se',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_sofort',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_paypal',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_stripe',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_payment_stub_proxy',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
-  {
-    name: 'rpc_checkout_micro',
-    options: { autoDelete: true, durable: false },
-    rpc: true,
-  },
   {
     name: 'async_events_transactions_micro',
     options: {
       durable: true,
-      deadLetterExchange: 'async_events_callback',
-      deadLetterRoutingKey: 'async_events_transactions_micro',
+      deadLetterExchange: 'async_events_fallback',
     },
     bindings: [
-      ...rabbitMqBindings,
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentActionCompleted,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentHistoryAdd,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentUpdated,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentCreated,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentRemoved,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentMigrate,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.BpoCreated,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.BpoUpdated,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentFlowCreated,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentFlowUpdated,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentFlowRemoved,
+      },
+      {
+        source: 'async_events',
+        routingKey: RabbitRoutingKeys.PaymentFlowMigrate,
+      },
     ],
   },
 ];
