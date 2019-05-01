909a439c564a9c061bea73a218f0e4ade813cd23
Alexey Sitka <alexey.sitka@gmail.com>
2019-05-01 12:43:29 +0200

    TM-134 Fixed dependencies.

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,6 +1,7 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotificationsSdkModule } from '@pe/notifications-sdk';
+import { environment } from '../environments';
 
 import { TransactionsEsExportCommand } from './command/transactions-export-to-es.command';
 import { TransactionsExportCommand } from './command/transactions-export.command';
@@ -29,7 +30,9 @@ import { StatisticsService } from './services/statistics.service';
 @Module({
   imports: [
     HttpModule,
-    NotificationsSdkModule,
+    NotificationsSdkModule.forRoot({
+      rabbitMqOptions: environment.rabbitmq,
+    }),
     MongooseModule.forFeature([{ name: 'Transaction', schema: TransactionsSchema }]),
     MongooseModule.forFeature([{ name: 'BusinessPaymentOption', schema: BusinessPaymentOptionSchema }]),
     MongooseModule.forFeature([{ name: 'PaymentFlow', schema: PaymentFlowSchema }]),
