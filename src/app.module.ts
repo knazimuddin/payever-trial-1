c151d3dad006dc1a337b84cc46a48dcdefed62c2
Alexey Sitka <alexey.sitka@gmail.com>
2019-06-12 14:55:24 +0200

    TM-150 Removed second ApmModule including.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ApmModule, CommandModule, JwtAuthModule, RabbitMqModule } from '@pe/nest-kit';
 import { NestKitLoggingModule } from '@pe/nest-kit/modules/logging';
 import { StatusModule } from '@pe/nest-kit/modules/status';
-
 import { environment } from './environments';
 import { TransactionsEsSearch } from './esTransactions/esTransactions.module';
 import { IntegrationModule } from './integration';
@@ -36,10 +35,6 @@ import { TransactionsModule } from './transactions/transactions.module';
     IntegrationModule,
     TransactionsModule,
     TransactionsEsSearch,
-    ApmModule.forRoot(
-      environment.apm.enable,
-      environment.apm.options,
-    ),
     CommandModule,
     RabbitMqModule.forRoot(environment.rabbitmq),
   ],
