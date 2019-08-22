322a976e9352b79adce27a53474ddb785d618799
Aleksey Egorov <countzero1981@gmail.com>
2019-08-22 16:39:49 +0300

    TM-196 2FA Changes

--- src/transactions/controllers/business.controller.ts
+++ src/transactions/controllers/business.controller.ts
@@ -13,7 +13,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse, ApiUseTags } from '@nestjs/swagger';
-import { ParamModel } from '@pe/nest-kit';
+import { ParamModel, SecondFactor } from '@pe/nest-kit';
 import { JwtAuthGuard, Roles, RolesEnum } from '@pe/nest-kit/modules/auth';
 import { QueryDto } from '@pe/nest-kit/modules/nest-decorator';
 
@@ -92,6 +92,7 @@ export class BusinessController {
   @Post(':uuid/action/:action')
   @HttpCode(HttpStatus.OK)
   @Roles(RolesEnum.merchant, RolesEnum.oauth)
+  @SecondFactor()
   public async runAction(
     @Param('action') action: string,
     @ParamModel(
@@ -118,6 +119,7 @@ export class BusinessController {
   @Post(':uuid/legacy-api-action/:action')
   @HttpCode(HttpStatus.OK)
   @Roles(RolesEnum.oauth)
+  @SecondFactor()
   public async runLegacyApiAction(
     @Param('action') action: string,
     @ParamModel(
