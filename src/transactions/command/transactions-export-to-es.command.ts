c59c63552560a90c07894d2e403fa39fc8cb6932
Alexey Sitka <alexey.sitka@gmail.com>
2019-08-26 17:34:17 +0200

    TM-198 Improved command for export to Elastic.

--- src/transactions/command/transactions-export-to-es.command.ts
+++ src/transactions/command/transactions-export-to-es.command.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Command } from '@pe/nest-kit';
+import { Command, Positional } from '@pe/nest-kit';
 import { Model } from 'mongoose';
 import { ElasticSearchClient } from '../elasticsearch/elastic-search.client';
 import { ElasticIndexingTextFieldsEnum, ElasticTransactionEnum } from '../enum';
@@ -14,13 +14,35 @@ export class TransactionsEsExportCommand {
   ) {}
 
   @Command({ command: 'transactions:export:es', describe: 'Export transactions for widgets' })
-  public async transactionsEsExport(): Promise<void> {
-    const count: number = await this.transactionsModel.countDocuments();
+  public async transactionsEsExport(
+    @Positional({
+      name: 'after',
+      type: 'string',
+    }) after: string,
+    @Positional({
+      name: 'before',
+      type: 'string',
+    }) before: string,
+  ): Promise<void> {
+    const criteria: any = {};
+    if (before || after) {
+      criteria.created_at = {};
+    }
+    if (before) {
+      criteria.created_at.$lte = new Date(before);
+    }
+    if (after) {
+      criteria.created_at.$gte = new Date(after);
+    }
+
+    const count: number = await this.transactionsModel.countDocuments(criteria);
+    Logger.log(`Found ${count} records.`);
+
     const limit: number = 200;
     let start: number = 0;
 
     while (start < count) {
-      const transactions: TransactionModel[] = await this.getWithLimit(start, limit);
+      const transactions: TransactionModel[] = await this.getWithLimit(start, limit, criteria);
       start += limit;
       Logger.log(`${transactions.length} items parsed`);
       await this.elasticSearchClient.bulkIndex(
@@ -43,9 +65,9 @@ export class TransactionsEsExportCommand {
     }
   }
 
-  private async getWithLimit(start: number, limit: number): Promise<any[]> {
+  private async getWithLimit(start: number, limit: number, criteria: any): Promise<any[]> {
     return this.transactionsModel.find(
-      {},
+      criteria,
       null,
       {
         limit: limit,
