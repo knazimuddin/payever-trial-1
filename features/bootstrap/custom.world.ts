f426131abe18028b24681d37d0a88e041f0e2005
a.stefanov <a.stefanov@petrosoftinc.com>
2019-09-04 11:36:23 +0200

    TM-189 Added cucumber tests

--- features/bootstrap/custom.world.ts
+++ features/bootstrap/custom.world.ts
@@ -2,8 +2,11 @@ import { AbstractWorld } from '@pe/cucumber-sdk';
 import { setWorldConstructor } from 'cucumber';
 import { AppModule } from '../../src/app.module';
 import { options } from './options';
+import { EmitterConsumerInitializer } from '../../src/transactions/emitter';
 
 export class CustomWorld extends AbstractWorld {
+  protected application: INestApplication;
+  protected consumersInitialized: boolean;
 
   public constructor({attach, parameters}) {
     super(
@@ -11,6 +14,14 @@ export class CustomWorld extends AbstractWorld {
       AppModule,
       options,
     );
+  }
+
+  public async init(scenario): Promise<void> {
+    await super.init(scenario);
+    if (!this.consumersInitialized) {
+      this.application.get(EmitterConsumerInitializer).init();
+      this.consumersInitialized = true;
+    }
   }
 }
 
