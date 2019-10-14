e3dda390e4ade1483e2258e183ffc15f200129fd
Alexey Sitka <alexey.sitka@gmail.com>
2019-10-14 10:08:49 +0200

    TM-247 CS.

--- src/transactions/services/payments-micro.service.ts
+++ src/transactions/services/payments-micro.service.ts
@@ -1,16 +1,16 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { MessageBusService, MessageInterface } from '@pe/nest-kit';
 
-import { environment } from '../../environments';
-
 const ORIGINAL_TIMEOUT: number = 15;
 
 @Injectable()
 export class PaymentsMicroService {
   private readonly stubMessageName: string = 'payment_option.stub_proxy.sandbox';
 
-  constructor(private readonly logger: Logger, private readonly messageBusService: MessageBusService) {
-  }
+  constructor(
+    private readonly logger: Logger,
+    private readonly messageBusService: MessageBusService,
+  ) {}
 
   public createPaymentMicroMessage(
     paymentType: string,
