36106c82812f581ea1e0967435da42a07f50e013
Lalit Singh <aerolalit@gmail.com>
2020-01-15 17:34:34 +0100

    TM-11 add unit tests for business payment service

---
+++ tests/unit/transactions/services/business-payment-options.service.spec.ts
@@ -0,0 +1,133 @@
+import 'mocha';
+
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+import { Logger } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { BusinessPaymentOptionModel } from '../../../../src/transactions/models';
+import { BusinessPaymentOptionService } from '../../../../src/transactions/services/business-payment-option.service';
+import { BusinessPaymentOptionInterface } from '../../../../src/transactions/interfaces';
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+const expect: Chai.ExpectStatic = chai.expect;
+
+describe('BusinessPaymentOption', () => {
+  let sandbox: sinon.SinonSandbox;
+  let testService: BusinessPaymentOptionService;
+  let businessPaymentModel: Model<BusinessPaymentOptionModel>;
+  let logger: Logger;
+
+  before(() => {
+    businessPaymentModel = {
+      findOne: (): any => { },
+      updateOne: (): any => { },
+    } as any;
+    logger = {
+      log: (): any => { },
+      warn: (): any => { },
+    } as any;
+
+    testService = new BusinessPaymentOptionService(
+      businessPaymentModel,
+      logger,
+    )
+
+  });
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+    sandbox = undefined;
+  });
+
+  describe('createOrUpdate()', async () => {
+    it('should create or update business payment model', async () => {
+      const dto: BusinessPaymentOptionInterface = {
+        credentials: [],
+        options: '{}',
+      } as any;
+
+      const bpo: BusinessPaymentOptionModel = {
+        id: 12345678,
+        toObject(): any {
+          return this;
+        },
+        options: '{}',
+      } as any;
+
+      sandbox.stub(businessPaymentModel, 'updateOne');
+      sandbox.stub(businessPaymentModel, 'findOne').resolves(bpo)
+      await testService.createOrUpdate(dto);
+    });
+
+    it('should create or update business payment model with illigal character in options', async () => {
+      const dto: BusinessPaymentOptionInterface = {
+        credentials: {},
+        options: {
+          bigInt: BigInt(9007199254740991),
+        },
+      } as any;
+
+      const bpo: BusinessPaymentOptionModel = {
+        id: 12345678,
+        toObject(): any {
+          return this;
+        },
+        options: '{{',
+      } as any;
+
+      sandbox.stub(businessPaymentModel, 'updateOne');
+      sandbox.stub(businessPaymentModel, 'findOne').resolves(bpo)
+      await testService.createOrUpdate(dto);
+    });
+
+    it('should create or update business payment model with no \'options\'', async () => {
+      const dto: BusinessPaymentOptionInterface = {
+        credentials: [],
+      } as any;
+
+      const bpo: BusinessPaymentOptionModel = {
+        id: 12345678,
+        toObject(): any {
+          return this;
+        },
+      } as any;
+
+      sandbox.stub(businessPaymentModel, 'updateOne');
+      sandbox.stub(businessPaymentModel, 'findOne').resolves(bpo)
+      await testService.createOrUpdate(dto);
+    });
+
+    it('should create or update business payment model with no \'options\'', async () => {
+      const dto: BusinessPaymentOptionInterface = {
+        credentials: [],
+      } as any;
+
+      const bpo: BusinessPaymentOptionModel = {
+        id: 12345678,
+        toObject(): any {
+          return this;
+        },
+      } as any;
+
+      sandbox.stub(businessPaymentModel, 'updateOne');
+      sandbox.stub(businessPaymentModel, 'findOne').resolves(bpo)
+      await testService.createOrUpdate(dto);
+    });
+  });
+
+  describe('findOneById()', async () => {
+    it('should find businessPaymentOptionModel by id', async () => {
+      sandbox.stub(businessPaymentModel, 'findOne').resolves(null);
+      expect(
+        await testService.findOneById(123),
+      ).to.eq(null);
+    });
+  });
+});
