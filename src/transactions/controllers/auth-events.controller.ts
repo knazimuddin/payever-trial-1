b9e87694b03db94197a2d503a566c45e49768006
rghorbani <r.ghorbani.f@gmail.com>
2021-01-07 09:32:29 +0100

    TM-130, Revert missing exec

--- src/transactions/controllers/auth-events.controller.ts
+++ src/transactions/controllers/auth-events.controller.ts
@@ -50,6 +50,6 @@ export class AuthEventsController {
     await this.transactionModel.updateMany(
       { _id: { $in: transactions.map((e: { _id: string}) => e._id)}, seller_name: { $exists: false }},
       { $set: { seller_name: sellerName, seller_email: data.email}},
-    ).exec();
+    );
   }
 }
