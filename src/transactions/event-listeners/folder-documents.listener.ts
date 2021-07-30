8eb086ad4a668d8210f5a784be89c87670b2f924
Dmytro Romanenko <Dmytro.Romanenko@payever.org>
2021-07-30 18:11:24 +0300

    TM-181 - cleaned up

--- src/transactions/event-listeners/folder-documents.listener.ts
+++ src/transactions/event-listeners/folder-documents.listener.ts
@@ -62,20 +62,16 @@ export class FolderDocumentsListener {
   public async elasticBeforeIndexDocument(
     elasticSearchElementDto: ElasticSearchElementDto,
   ): Promise<void> {
-    console.log('before ElasticBeforeIndexDocument', elasticSearchElementDto)
     elasticSearchElementDto.document.amount = Math.trunc(elasticSearchElementDto.document.amount * 100);
     elasticSearchElementDto.document.total = Math.trunc(elasticSearchElementDto.document.total * 100);
-    console.log('aftre ElasticBeforeIndexDocument', elasticSearchElementDto)
   }
 
   @EventListener(FoldersEventsEnum.ElasticProcessSearchResult)
   public async elasticProcessSearchResult(
     elasticSearchElementDto: ElasticSearchElementDto,
   ): Promise<void> {
-    console.log('before ElasticProcessSearchResult', elasticSearchElementDto)
     elasticSearchElementDto.document.amount = elasticSearchElementDto.document.amount / 100;
     elasticSearchElementDto.document.total = elasticSearchElementDto.document.total / 100;
-    console.log('after ElasticProcessSearchResult', elasticSearchElementDto)
   }
 
   @EventListener(FoldersEventsEnum.ElasticGetAdditionalSearchResults)
@@ -83,8 +79,6 @@ export class FolderDocumentsListener {
     additionalResults: ElasticAdditionalSearchResultsDto,
   ): Promise<void> {
     const amount: number = await this.totalAmount(additionalResults.elasticFilters, additionalResults.currency);
-
-    console.log(additionalResults.elasticFilters);
 
     additionalResults.paginationData = {
       amount,
