35a3cfb02323331da264db5ce5610f7b57747218
Dmytro Romanenko <Dmytro.Romanenko@payever.org>
2022-02-10 17:35:29 +0200

    TM-256 - tmp logs

--- src/transactions/services/transactions.service.ts
+++ src/transactions/services/transactions.service.ts
@@ -302,6 +302,7 @@ export class TransactionsService {
     refundItems: TransactionCartItemInterface[],
   ): Promise<void> {
     console.log('saveRefundItems', refundItems);
+    console.log('transaction.refunded_items', transaction.refunded_items);
 
     refundItems.forEach((refundItem: TransactionCartItemInterface) => {
       const existingRefundItem: TransactionCartItemInterface =
