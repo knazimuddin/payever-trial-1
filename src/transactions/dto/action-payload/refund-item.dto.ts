50e7f063a947fe8fee8d86584d8a4534f1a1845f
Dmytro Romanenko <Dmytro.Romanenko@payever.org>
2022-02-10 15:33:04 +0200

    TM-256 - handled item_uuid

--- src/transactions/dto/action-payload/refund-item.dto.ts
+++ src/transactions/dto/action-payload/refund-item.dto.ts
@@ -1,5 +1,8 @@
 import { IsNumber, IsString } from 'class-validator';
 
+/**
+ * @deprecated The class should not be used
+ */
 export class RefundItemDto {
   @IsString()
   public paymentItemId: string;
