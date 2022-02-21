b3070fd83f2170832d3b26e3a13344408c4a5115
rkhoshgoftar <rkhoshgoftar@gmail.com>
2022-02-21 15:25:19 +0330

    create translation service

--- src/transactions/services/translation.service.ts
+++ src/transactions/services/translation.service.ts
@@ -1,34 +1,74 @@
 import { Injectable, Logger, HttpException } from '@nestjs/common';
-import { HttpService } from '@nestjs/axios';
+import { IntercomService } from "@pe/nest-kit";
+import { ConfigService } from "@nestjs/config";
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class TranslationService {
 
-  constructor(private httpService: HttpService) {}
+  private readonly translationUrl: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly logger: Logger,
+    private readonly httpService: IntercomService) {
+      this.translationUrl = this.configService.get<string>("MICRO_Translation_Service");
+    }
 
   public async translate(keys: string[], langCode: string): Promise<string[]> {
     return this.convert(keys, langCode);
   }
 
   private async convert(keys: string[], langCode: string): Promise<string[]> {
-    const url = `https://payeverstaging.azureedge.net/translations/frontend-transactions-app-${langCode}.json`;
-    let result: string[] = [];
-    return new Promise<string[]>((resolve, reject) => {
-      this.httpService.get(url).subscribe(response => {
-        if(response.status == 200){
-          let data = response.data;
-          for (let index = 0; index < keys.length; index++) {
-            const key = keys[index];
-            if(data[key])
-              result.push(data[key]);
-          }
-          resolve(result);
-        }
-        else
-          reject(response.statusText);
-      });
-    });
+    const url = this.translationUrl.replace('{{lang}}', langCode);
+    let result = [];
+    let data = await this.httpService.get(url);
 
+    return data.pipe(
+      map(response => {
+          const apiData = response.data;
+          const result = [];
+
+          if (!apiData) {
+            this.logger.warn(`keys is empty`);
+            return result;
+          };
+
+          keys.forEach(key => {
+            result[key] = apiData[key] || key;
+          });
+
+          return result;
+      }),
+      catchError(err => {
+          this.logger.error({
+              error: err.response.data,
+              message: 'Translate api failed',
+              url: url,
+          });
+      })).toPromise();
   }
+
+
+  // private async convert(keys: string[], langCode: string): Promise<string[]> {
+  //   const url = `https://payeverstaging.azureedge.net/translations/frontend-transactions-app-${langCode}.json`;
+  //   let result: string[] = [];
+  //   return new Promise<string[]>((resolve, reject) => {
+  //     this.httpService.get(url).subscribe(response => {
+  //       if(response.status == 200){
+  //         let data = response.data;
+  //         for (let index = 0; index < keys.length; index++) {
+  //           const key = keys[index];
+  //           if(data[key])
+  //             result.push(data[key]);
+  //         }
+  //         resolve(result);
+  //       }
+  //       else
+  //         reject(response.statusText);
+  //     });
+  //   });
+
+  // }
 
 }
---
+++ tests/unit/transactions/services/translation.service.spect.ts
@@ -0,0 +1,77 @@
+import { TranslationService } from "../../../../src/transactions/services/translation.service";
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { IntercomService } from '@pe/nest-kit';
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import * as sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+const expect: Chai.ExpectStatic = chai.expect;
+
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
+
+
+describe("TranslationService", () => {
+
+    let sandbox: sinon.SinonSandbox;
+    let translationService: TranslationService;
+    let configService: ConfigService;
+    let logger: Logger;
+    let httpService: IntercomService;
+
+    before(() => {
+        httpService = {
+            get: (): any => { },
+        } as any;
+
+        logger = {
+            error: (error): any => { console.error(error) },
+            log: (): any => { },
+            warn: (data): any => { console.warn(data) },
+        } as any;
+
+        configService = {
+            get: (): any => true,
+        } as any;
+
+
+        translationService = new TranslationService(configService, logger, httpService)
+    });
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+        sandbox = undefined;
+    });
+
+    describe("translate()", () => {
+        it("return array of translation", async () => {
+
+            const getResponse: [] = ["actions.back": "Back", "actions.cancel": "Cancel" ];
+
+            const response: Observable<AxiosResponse<any>> = {
+                pipe: (fn): any => { return fn() },
+            } as any;
+
+            const responseData: Observable<any> = {
+                toPromise: (a): any => { return a },
+            } as any;
+
+            sandbox.stub(httpService, 'get').resolves(response);
+            sandbox.stub(response, 'pipe').returns(responseData);
+            sandbox.stub(responseData, 'toPromise').resolves(getResponse);
+
+            var res = await translationService.getTranslation(["actions.back", "actions.cancel"], "en")
+            expect(res).to.equal(getResponse)
+        })
+    })
+
+
+})
